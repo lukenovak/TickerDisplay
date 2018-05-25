e17c33fefb985810707fa95d6dbea6613c18b03d
Luke Novak <luken22@optonline.net>
2018-05-24 23:42:33 -0400

    condensed code, version bump

--- display.js
+++ display.js
@@ -1,33 +1,8 @@
-// Represents a Post, currently not used
-function Post(title) {
-  var title = title;
-}
-
 // String of length = 1 -> Boolean
 // is this character a letter in the latin alphabet?
 function isLatinLetter(letter) {
   letter += "";
   return (letter.length === 1 && letter.match(/[a-z]/i));
-}
-
-// String -> Boolean
-// does the string contain a stock?
-// a stock is defined as a sequence of the following:
-// A dollar sign ("$") followed by a sequence of 1-5 letters
-function containsStock(title) {
-  // making sure that title is a string
-  title += "";
-  // has a stock been found?
-  var stockFound = false;
-  for (var i = 0; i < title.length; i= i + 1) {
-    if (title.charAt(i) == "$") {
-      if (isLatinLetter(title.charAt(i + 1))) {
-        stockFound = true;
-        break;
-      }
-    }
-  }
-  return stockFound;
 }
 
 // identifies the first stock in the post
@@ -55,74 +30,27 @@ function stockNameInPost(title) {
     return ticker;
 }
 
-// gets the tickers from a post only if it has one
-function getTickersIfPresent(title) {
-  if (containsStock(title)) {
-    return stockNameInPost(title);
-  }
-  else return "No stocks in this post";
-}
-
-// Post -> String -> Number
-// how many days ago was this post made
-function getDate() {
-  var date = $("p.tagline time").attr("title");
-  // parses the date based on what date has fed it
-  // ** see Date.parse documentation for more info here **
-  return Date.parse(date);
-}
-
-// gets the current date and formats for the api
-function formattedDate() {
-  // generation of the date and getting the data to be formatted
-  var today = new Date();
-  var day = today.getDate();
-  var month = today.getMonth() + 1;
-  var year = today.getFullYear();
-
-  // adds the 0 before the month if it's only one digit
-  if (month < 10) {
-    month = "0" + month;
-  }
-
-  // formatting the date
-  var dateInFormat = year + "-" + month + "-" + day;
-  return dateInFormat;
-}
-
 // gets the current price of a given stock
 // the input ticker must be a string
-function getPrice(ticker, post) {
-  var html = "no price present"
+function getAndDisplayPrice(title, post) {
+  var ticker = stockNameInPost(title)
   var defaultPrice = 1;
   var url = 'https://api.iextrading.com/1.0/stock/' +
             ticker + '/quote';
   // gets the JSON
-  var priceJSON = $.getJSON(url, function(data) {
-    // this code only runs on success
-    // calls the function that displays the price
-    displayPrice(data, post);
-    });
-  return priceJSON;
-}
-
-// gets the current price of a ticker if there is one
-function getPriceIfPresent(title, post) {
-  var ticker = getTickersIfPresent(title);
-  if (ticker !== "No stocks in this post") {
-    var price = getPrice(ticker, post);
-    return price;
-  }
-  else {
-    return "No stocks in this post";
-  }
+  if (ticker.length > 0) {
+    var priceJSON = $.getJSON(url, function(data) {
+      // this code only runs on success
+      // calls the function that displays the price
+      displayPrice(data, post);
+      defaultPrice = data.latestPrice;
+      return defaultPrice;
+      });
+    }
 }
 
 // returns the box that should be displayed for the
 function displayPrice(data, post) {
-  if (post == null) {
-    return;
-  }
   var price = data.latestPrice;
   var ticker = data.symbol;
   // are we representing our data as percents or dollars
@@ -157,7 +85,7 @@ function buildTickerBox(price, ticker, change, isPercent) {
     //adds the div wrapper
     htmlString = htmlString + "stock-up\">";
   }
-  if (change <= 0) {
+  else {
     htmlString = htmlString + "stock-down\">";
   }
   // adds the text inside the divider
@@ -184,20 +112,19 @@ function percentChangeString(percentChange) {
 // creates a dollar change string from dollar change data
 function dollarChangeString(dollarChange) {
   dollarChange = Number(dollarChange.toFixed(2)); //rounds to the cent
-  var dollarString = "$";
+  var dollarString;
   if (dollarChange > 0) {
-    dollarString = "+ " + dollarString + dollarChange;
+    dollarString = "+ $" + dollarChange;
   }
   else {
     dollarChange = dollarChange * -1;
-    dollarString = "- " + dollarString + dollarChange;
+    dollarString = "- $" + dollarChange;
   }
   return dollarString;
 }
 
-
 // function call that calls the function to display the price
 $(".top-matter").each(function(index, value) {
-  var title = $(this).find($("p.title a[data-event-action = title]")).text()
-  var toBeInserted = getPriceIfPresent(title, $(this));
+  var post = $(this).find($("p.title a[data-event-action = title]"));
+  var toBeInserted = getAndDisplayPrice(post.text(), $(this));
 });
